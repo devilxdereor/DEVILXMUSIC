e158ae635fa2af1a0bb6020b1b168b4db9bd1d63
FC DEVIL MUSIC <104202678+devilxdereor@users.noreply.github.com>
2022-04-24 17:53:02 +0530

    Create __init__.py

---
+++ DEVILMUSIC/utils/inline/__init__.py
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2021-2022 by devilxdereor@Github, < https://github.com/devilxdereor >.
+#
+# This file is part of < https://github.com/devilxdereor/DEVILXMUSIC > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/devilxdereor/DEVILXMUSIC/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from .help import *
+from .play import *
+from .playlist import *
+from .queue import *
+from .settings import *
+from .song import *
+from .start import *
