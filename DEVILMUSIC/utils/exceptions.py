8649dd76130f000ddbc063f780ce76b3f6ce12be
FC DEVIL MUSIC <104202678+devilxdereor@users.noreply.github.com>
2022-04-24 14:46:20 +0530

    Create exceptions.py

---
+++ DEVILMUSIC/utils/exceptions.py
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021-2022 by devilxdereor@Github, < https://github.com/devilxdereor >.
+#
+# This file is part of < https://github.com/devilxdereor/DEVILXMUSIC > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/devilxdereor/DEVILXMUSIC/blob/master/LICENSE >
+#
+# All rights reserved.
+
+
+class AssistantErr(Exception):
+    def __init__(self, errr: str):
+        super().__init__(errr)
