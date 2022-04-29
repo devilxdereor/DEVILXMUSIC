53f92a41ced2d4a9554a748a951be66384d39ec4
FC DEVIL MUSIC <104202678+devilxdereor@users.noreply.github.com>
2022-04-29 17:02:53 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,9 +1,9 @@
 #
-# Copyright (C) 2021-2022 by devilxdereor@Github, < https://github.com/Devilxdereor >.
+# Copyright (C) 2021-2022 by devilxdereor@Github, < https://github.com/devilxdereor >.
 #
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# This file is part of < https://github.com/devilxdereor/DEVILXMUSIC > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+# Please see < https://github.com/devilxdereor/DEVILXMUSIC/blob/master/LICENSE >
 #
 # All rights reserved.
 
